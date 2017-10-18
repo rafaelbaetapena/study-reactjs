dc2f50bbfc3724ce58cddd1d6fee22f9132a91b2
Rafael Pena <rafaelrbncp@gmail.com>
2017-10-18 15:56:34 -0200

    my-app

---
+++ my-app/src/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import './index.css';
+// import App from './App';
+// import registerServiceWorker from './registerServiceWorker';
+
+function Welcome(props) {
+    return <h1>Hello, {props.name}</h1>;
+  }
+  
+  function App() {
+    return (
+      <div>
+        <Welcome name="Sara" />
+        <Welcome name="Cahal" />
+        <Welcome name="Editee" />
+      </div>
+    );
+  }
+
+ReactDOM.render(<App />, document.getElementById('root'));
+//registerServiceWorker();
