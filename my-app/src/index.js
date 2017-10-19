712703e85adb8924fb31a49b195a423624ed3c79
unknown <rafaelrbncp@gmail.com>
2017-10-19 15:00:37 -0200

    get started

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -6,18 +6,23 @@ import Welcome from './Welcome';
 import Avatar from './Avatar';
 import UserInfo from './UserInfo';
 import Comment from './Comment'
-import Clock from './Clock'
+import Clock from './Clock';
+import ActionLink from './ActionLink';
+import Toggle from './Toggle';
+import LoggingButton from './LoggingButton';
 // import registerServiceWorker from './registerServiceWorker';
   
-
 function App() {
     return (
       <div>
+        <Toggle />
+        <ActionLink />
         <Clock />
         <Welcome name="Sara" />
         <Welcome name="Cahal" />
         <Welcome name="Editee" />
         <Clock />
+        <LoggingButton />
       </div>
     );
 }
