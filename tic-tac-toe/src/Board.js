135a13f9d5fd022d6ee3ceb65c969ee1f1aa6943
unknown <rafaelrbncp@gmail.com>
2017-10-20 18:35:16 -0200

    game, board and square

---
+++ tic-tac-toe/src/Board.js
@@ -0,0 +1,51 @@
+import React from 'react';
+
+import Square from './Square';
+
+class Board extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      squeres: Array(9).fill(null)
+    }
+  }
+
+    renderSquare(i) {
+      return <Square 
+        value={this.state.squeres[i]} 
+        onClick={() => this.handleClick(i)} />;
+    }
+
+    handleClick(i){
+      const squeres = this.state.squeres.slice();
+      squeres[i] = 'X';
+      this.setState({squeres: squeres});
+    }
+  
+    render() {
+      const status = 'Next player: X';
+  
+      return (
+        <div>
+          <div className="status">{status}</div>
+          <div className="board-row">
+            {this.renderSquare(0)}
+            {this.renderSquare(1)}
+            {this.renderSquare(2)}
+          </div>
+          <div className="board-row">
+            {this.renderSquare(3)}
+            {this.renderSquare(4)}
+            {this.renderSquare(5)}
+          </div>
+          <div className="board-row">
+            {this.renderSquare(6)}
+            {this.renderSquare(7)}
+            {this.renderSquare(8)}
+          </div>
+        </div>
+      );
+    }
+  }
+
+export default Board;
