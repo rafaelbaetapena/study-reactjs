23f62b5866c1babf37b87dedb248af13e250e77f
Rafael Pena <rafaelrbncp@gmail.com>
2018-03-01 18:45:53 -0300

    getList

--- my-money-app/frontend/src/billingCycle/billingCycleList.jsx
+++ my-money-app/frontend/src/billingCycle/billingCycleList.jsx
@@ -1,8 +1,26 @@
 import React, { Component } from 'react'
+import { bindActionCreators } from 'redux'
+import { connect } from "react-redux"
+import { getList } from "../billingCycle/billingCycleActions"
 
 class BillingCycleList extends Component {
 
-    render(){
+    componentWillMount() {
+        this.props.getList()
+    }
+
+    renderRows() {
+        const list = this.props.list || []
+        return list.map(bc => (
+            <tr key={bc._id}>
+                <td>{bc.name}</td>
+                <td>{bc.month}</td>
+                <td>{bc.year}</td>
+            </tr>
+        ))
+    }
+
+    render() {
         return (
             <div>
                 <table className='table'>
@@ -14,7 +32,7 @@ class BillingCycleList extends Component {
                         </tr>
                     </thead>
                     <tbody>
-
+                        {this.renderRows()}
                     </tbody>
                 </table>
             </div>
@@ -22,4 +40,6 @@ class BillingCycleList extends Component {
     }
 }
 
-export default BillingCycleList
+const mapStateToProps = state => ({ list: state.billingCycle.list })
+const mapDispatchToProps = dispatch => bindActionCreators({ getList }, dispatch)
+export default connect(mapStateToProps, mapDispatchToProps)(BillingCycleList)
